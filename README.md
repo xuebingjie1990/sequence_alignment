2129450d138764aaa95ed8f30f7cc8e304a21156
Aakrosh Ratan <ratan@bx.psu.edu>
2021-12-31 14:12:10 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# alignment
+# Aligning sequences
+
+## Implement the Smith-Waterman algorithm to align two sequences.
+
+For this assignment, implement the `smith_waterman` function in align_sequences.py. The function takes in two nucleotide sequences (seq1 and seq2), and scores/penalty for a match, mismatch, opening of a new gap, and extension of a gap. You should write the `smith_waterman` function to calculate the maximum score of the local alignment and the resulting aligned subsequences from the two input sequences. For example if seq1 is `ACGTTA` and seq2 is `ACTTA`, match=1, mismatch=1, gapopen=1, gapextend=0, then the expected output of the function will be `4, 'ACGTTA','AC-TTA'`
+
+## Testing
+
+You can test your work by running:
+
+```
+python3 testdriver
+```
---
+++ align_sequences.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+"""
+    usage:
+        align_sequences [options] seq1.fa seq2.fa
+    where the options are:
+        -h,--help : print usage and quit
+        -m,--match: score of a match in the alignment [2]
+        -x,--mismatch: penalty for a mismatch in the alignment [1]
+        -g,--gapopen: penalty for opening a new gap [4]
+        -e,--gapextend: penalty for extending a gap [1]
+"""
+
+from sys import argv, stderr
+from getopt import getopt, GetoptError
+
+def read_single_contig_fasta(filename):
+    names = []
+    sequences = []
+    with open(filename, 'r') as f:
+        line = f.readline()
+        assert line.startswith(">")
+        names.append(line.strip().split("\t"))
+        sequence = ""
+        for line in f:
+            if line.startswith(">"):
+                sequences.append(sequence)
+                names.append(line.strip().split("\t"))
+                sequence = ""
+            else:
+                for x in line.strip():
+                    if x not in ["A", "C", "G", "T"]:
+                        print("Unknown nucleotide {}".format(x), file=stderr)
+                        exit(3)
+                sequence += line.strip()
+
+    sequences.append(sequence)
+    assert len(names) == 1
+    assert len(sequences) == 1
+    return names[0], sequences[0]
+
+def smith_waterman(seq1, seq2, match, mismatch, gapopen, gapextend):
+    max_score = 0
+    alnseq1 = ""
+    alnseq2 = ""
+
+
+    return max_score, alnseq1, alnseq2
+    
+
+def main(filename1, filename2, match, mismatch, gapopen, gapextend):
+    name1, seq1 = read_single_contig_fasta(filename1)
+    name2, seq2 = read_single_contig_fasta(filename2)
+
+    max_score, alnseq1, alnseq2 = smith_waterman(seq1, seq2, 
+                                  match, mismatch, gapopen, gapextend)
+    
+    print("Maximum alignment score: {}".format(max_score))
+    print("Sequence1 : {}".format(alnseq1))
+    print("Sequence2 : {}".format(alnseq2))
+
+if __name__ == "__main__":
+    try:
+        opts, args = getopt(argv[1:],
+                     "hm:x:g:e:",
+                     ["help", "match=", "mismatch=", "gapopen=", "gapextend="])
+    except GetoptError as err:
+        print(err)
+        print(__doc__, file=stderr)
+        exit(1) 
+
+    match = 2
+    mismatch = 1
+    gapopen = 4
+    gapextend = 1
+
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            print(__doc__, file=stderr)
+            exit()
+        elif o in ("-m", "--match"):
+            match = float(a)
+        elif o in ("-x", "--mismatch"):
+            mismatch = float(a)
+        elif o in ("-g", "--gapopen"):
+            gapopen = float(a)
+        elif o in ("-e", "--gapextend"):
+            gapextend = float(a)
+        else:
+            assert False, "unhandled option"
+
+    if len(args) != 2:
+        print(__doc__, file=stderr)
+        exit(2)
+
+    main(args[0], args[1], match, mismatch, gapopen, gapextend)
